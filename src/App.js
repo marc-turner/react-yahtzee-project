f245a55179ba34779b55b0f3c5fd82577df41ad2
Marc Turner <marc.t1967@gmail.com>
2020-05-15 20:08:15 -0400

    add 1st fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import Game from "./Game";
-import "./App.css";
+import React, { Component } from 'react';
+import Game from './Game';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Game />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <Game />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import './RuleRow.css'
+import './RuleRow.css';
 
 class RuleRow extends Component {
-  render() {
-    return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
-      </tr>
-    )
-  }
+    render() {
+        return (
+            <tr className='RuleRow RuleRow-active' onClick={this.props.doScore}>
+                <td className='RuleRow-name'>{this.props.name}</td>
+                <td className='RuleRow-score'>{this.props.score}</td>
+            </tr>
+        );
+    }
 }
 
 export default RuleRow;
